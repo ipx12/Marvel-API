949d2d8e983402a9119866b4495b4690a860be41
Denis <example@gmail.com>
2021-11-25 10:55:46 +0200

    add random char, and charList

--- src/index.js
+++ src/index.js
@@ -1,14 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/App';
-import MarvelService from './services/MarvelService';
 
 import './style/style.scss';
 
-const marvelServise = new MarvelService();
 
-marvelServise.getCharacter(1010338).then(res => res.data.results.forEach(item => console.log(item.name)));
-marvelServise.getAllCharacters().then(res => res.data.results.forEach(item => console.log(item.name)));
 
 ReactDOM.render(
   <React.StrictMode>
--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -14,12 +14,29 @@ class MarvelService {
         return await res.json();
     }
 
-    getAllCharacters = () => {
-        return this.getResourse(`${this._apiBse}characters?limit=9&offset=210&${this._apiKey}`);
+    getAllCharacters = async () => {
+        const res = await this.getResourse(`${this._apiBse}characters?limit=9&offset=210&${this._apiKey}`);
+        return res.data.results.map(this._transformCharacter);
     }
 
-    getCharacter = (id) => {
-        return this.getResourse(`${this._apiBse}characters/${id}?${this._apiKey}`);
+    getCharacter = async (id) => {
+        const res = await this.getResourse(`${this._apiBse}characters/${id}?${this._apiKey}`);
+        return this._transformCharacter(res.data.results[0]);
+    }
+
+    _transformCharacter = (char) => {
+        let description = char.description ? char.description : 'No data'; 
+        if (description.length > 150) {
+            description = description.slice(0, 150) + '..!';
+        }
+        return {
+            name: char.name,
+            description: description,
+            thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
+            homepage: char.urls[0].url,
+            wiki: char.urls[1].url,
+            id: char.id
+        }
     }
 }
 
